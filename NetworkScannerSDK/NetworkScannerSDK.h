0a24283ea4703fd63843ee79560dfce9d5cd199c
Kryakalka <kuanyshbekovalisher57@gmail.com>
2024-06-05 15:14:18 +0500

    Initial Commit

---
+++ NetworkScannerSDK/NetworkScannerSDK.h
@@ -0,0 +1,18 @@
+//
+//  NetworkScannerSDK.h
+//  NetworkScannerSDK
+//
+//  Created by Alisher on 05.06.2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NetworkScannerSDK.
+FOUNDATION_EXPORT double NetworkScannerSDKVersionNumber;
+
+//! Project version string for NetworkScannerSDK.
+FOUNDATION_EXPORT const unsigned char NetworkScannerSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetworkScannerSDK/PublicHeader.h>
+
+
